a3d6c0e4ca803d9f93869ddb2b32ab1f1033bb23
yiming <yiming.zheng@mail.utoronto.ca>
2023-09-08 10:56:27 -0400

    remove duplicate defines in DPUBackEnd.h

--- DPDPU/StorageEngine/DDSBackEndDPUService/Include/DPUBackEnd.h
+++ DPDPU/StorageEngine/DDSBackEndDPUService/Include/DPUBackEnd.h
@@ -10,29 +10,29 @@
 #include "spdk/log.h"
 #include "spdk/string.h"
 #include "spdk/bdev_zone.h"
-// #include "../../../Common/Include/DDSTypes.h"
+#include "../../../Common/Include/DDSTypes.h"
 
-#define DDS_MAX_DIRS 1000
-#define DDS_MAX_FILE_PATH 64
-#define DDS_MAX_FILES 1000
+// #define DDS_MAX_DIRS 1000
+// #define DDS_MAX_FILE_PATH 64
+// #define DDS_MAX_FILES 1000
 #define DDS_MAX_FILES_PER_DIR 1000
-#define DDS_DIR_INVALID -1
-#define DDS_DIR_ROOT 0
-#define DDS_FILE_INVALID -1
-#define DDS_PAGE_SIZE 4096
+// #define DDS_DIR_INVALID -1
+// #define DDS_DIR_ROOT 0
+// #define DDS_FILE_INVALID -1
+// #define DDS_PAGE_SIZE 4096
 
-#define DDS_ERROR_CODE_SUCCESS 0
-#define DDS_ERROR_CODE_TOO_MANY_DIRS 1
-#define DDS_ERROR_CODE_TOO_MANY_FILES 2
-#define DDS_ERROR_CODE_DIR_EXISTS 3
-#define DDS_ERROR_CODE_DIR_NOT_FOUND 4
-#define DDS_ERROR_CODE_DIR_NOT_EMPTY 5
-#define DDS_ERROR_CODE_FILE_EXISTS 6
-#define DDS_ERROR_CODE_FILE_NOT_FOUND 7
-#define DDS_ERROR_CODE_READ_OVERFLOW 8
-#define DDS_ERROR_CODE_WRITE_OVERFLOW 9
-#define DDS_ERROR_CODE_STORAGE_OUT_OF_SPACE 16
-#define DDS_ERROR_CODE_INVALID_FILE_POSITION 17
+// #define DDS_ERROR_CODE_SUCCESS 0
+// #define DDS_ERROR_CODE_TOO_MANY_DIRS 1
+// #define DDS_ERROR_CODE_TOO_MANY_FILES 2
+// #define DDS_ERROR_CODE_DIR_EXISTS 3
+// #define DDS_ERROR_CODE_DIR_NOT_FOUND 4
+// #define DDS_ERROR_CODE_DIR_NOT_EMPTY 5
+// #define DDS_ERROR_CODE_FILE_EXISTS 6
+// #define DDS_ERROR_CODE_FILE_NOT_FOUND 7
+// #define DDS_ERROR_CODE_READ_OVERFLOW 8
+// #define DDS_ERROR_CODE_WRITE_OVERFLOW 9
+// #define DDS_ERROR_CODE_STORAGE_OUT_OF_SPACE 16
+// #define DDS_ERROR_CODE_INVALID_FILE_POSITION 17
 #define DDS_ERROR_CODE_SEGMENT_RETRIEVAL_FAILURE 18
 #define DDS_ERROR_CODE_RESERVED_SEGMENT_ERROR 19
 #define DDS_ERROR_CODE_OUT_OF_MEMORY 20
@@ -55,14 +55,14 @@
 // Types used in DPU
 //
 //
-typedef char* BufferT;
-typedef void* ContextT;
-typedef int DirIdT;
-typedef int ErrorCodeT;
-typedef unsigned long FileAttributesT;
-typedef int FileIdT;
-typedef unsigned long FileIOSizeT;
-typedef unsigned long long FileSizeT;
+// typedef char* BufferT;
+// typedef void* ContextT;
+// typedef int DirIdT;
+// typedef int ErrorCodeT;
+// typedef unsigned long FileAttributesT;
+// typedef int FileIdT;
+// typedef unsigned long FileIOSizeT;
+// typedef unsigned long long FileSizeT;
 typedef unsigned long long DiskSizeT;
 typedef int SegmentIdT;
 typedef unsigned int SegmentSizeT;
