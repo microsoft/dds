6011a547c25893621fb6937a7bd47262ca50dd67
Jason Hu <hujason@hotmail.com>
2023-09-18 06:38:34 -0400

    Running FS on a separate pthread should work. And also spdk app on a separate pthread will work with spdk threads.

--- Samples/Tests/test_with_main_loop.c
+++ Samples/Tests/test_with_main_loop.c
@@ -1,11 +1,6 @@
 #include "../../DPDPU/StorageEngine/DDSBackEndDPUService/Include/DPUBackEndStorage.h"
 #include "../../DPDPU/StorageEngine/DDSBackEndDPUService/Include/bdev.h"
 
-#include <pthread.h>
-#include "spdk/thread.h"
-
-SPDKContextT *SPDKContext;
-
 static void
 dds_custom_args_usage(void)
 {
@@ -27,30 +22,12 @@ struct mythread_ctx {
     SyncRWCompletionStatus cookie;
 };
 
-struct app_start_args {
-    struct spdk_app_opts *opts;
-    void (*app_start_wrapper_func) (void *);
-    void *ctx;
-};
-
-struct app_thread_ctx {
-    struct spdk_thread **spdk_threads;
-};
-
-
 
 void io1_callback_func(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg) {
     SPDK_NOTICELOG("IO 1 Callback running...\n");
     SyncRWCompletionStatus *status = cb_arg;
     *status = success ? 1 : 2;
     SPDK_NOTICELOG("IO 1 Callback finished, set status %p to %d\n", status, *status);
-    SPDK_NOTICELOG("thread work finished... stopping app...\n");
-    // XXX: MUST release resources, or thread exit will be broken, and app WON'T stop!!
-    spdk_bdev_close(SPDKContext->bdev_desc);
-    spdk_put_io_channel(SPDKContext->bdev_io_channel);
-
-    spdk_thread_exit(spdk_get_thread());
-    spdk_app_stop(0);
 }
 
 void io2_callback_func(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg) {
@@ -61,24 +38,9 @@ void io2_callback_func(struct spdk_bdev_io *bdev_io, bool success, void *cb_arg)
 }
 
 
-void thread_work_func(void *thread_ctx) {
-    SPDK_NOTICELOG("DOING WORK A...\n");
-
-    void *tmpbuf = malloc(4096);
-    SyncRWCompletionStatus cookie = 777;
-    int rc = bdev_read(SPDKContext, tmpbuf, 0, 1024, io1_callback_func, &(cookie));
-    SPDK_NOTICELOG("bdev_read() rc: %d\n", rc);
-
-    SPDK_NOTICELOG("DOING WORK B...\n");
-    // SPDK_NOTICELOG("thread work finished... stopping app...\n");
-    // spdk_app_stop(0);
-}
-
 void TestBackEnd(
     void *args
 ) {
-    struct app_thread_ctx *spdk_app_ctx = args;
-    spdk_app_ctx->spdk_threads[0] = spdk_thread_create("thread0", NULL);
     //
     // Initialize SPDK stuff
     //
@@ -116,7 +78,7 @@ void TestBackEnd(
     SyncRWCompletionStatus *cb_arg = &(myctx->cookie);
     printf("cb_arg ptr: %p, value: %hu\n", cb_arg, *cb_arg);
 
-    while (0) {
+    while (1) {
         SPDK_NOTICELOG("DOING WORK A...\n");
 
         SPDK_NOTICELOG("DOING IO 1..., status ptr: %p, value: %hu\n", &(myctx->cookie), myctx->cookie);
@@ -133,77 +95,37 @@ void TestBackEnd(
         SPDK_NOTICELOG("FINISHED IO 2..., status ptr: %p, value: %hu\n", &(myctx->cookie), myctx->cookie);
 
         SPDK_NOTICELOG("DOING WORK C...\n");
-        sleep(1);
     }
+    
 
-    /* SPDK_NOTICELOG("ALL WORK DONE, STOPPING...\n");
+
+    
+
+
+    SPDK_NOTICELOG("ALL WORK DONE, STOPPING...\n");
     spdk_bdev_close(spdkContext->bdev_desc);
     spdk_put_io_channel(spdkContext->bdev_io_channel);
-    spdk_app_stop(0); */
-
-    SPDKContext = spdkContext;
-    spdk_thread_send_msg(spdk_app_ctx->spdk_threads[0], thread_work_func, NULL);
-    SPDK_NOTICELOG("SPDK MAIN ended but not stopped, also has spdk threads spawned...\n");
+    spdk_app_stop(0);
 }
 
-
-void app_start_wrapper_func(void *ctx) {
-    struct app_start_args *args = ctx;
-    SPDK_NOTICELOG("app_start_wrapper_func() called\n");
-    int rc = spdk_app_start(args->opts, args->app_start_wrapper_func, args->ctx);  // block until `spdk_app_stop` is called
-    SPDK_NOTICELOG("app_start_wrapper_func() returned: %d\n", rc);
-    return rc;
-}
 
 int main(int argc, char **argv) {
     int rc;
-    // struct spdk_app_opts opts = {};
+    struct spdk_app_opts opts = {};
     /* Set default values in opts structure. */
-	// spdk_app_opts_init(&opts, sizeof(opts));
-	// opts.name = "hello_bdev";
+	spdk_app_opts_init(&opts, sizeof(opts));
+	opts.name = "hello_bdev";
     /*
 	 * Parse built-in SPDK command line parameters as well
 	 * as our custom one(s).
 	 */
-	/* if ((rc = spdk_app_parse_args(argc, argv, &opts, "b:", NULL, dds_parse_arg,
-				      dds_custom_args_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
-        printf("spdk_app_parse_args() failed with: %d\n", rc);
-		exit(rc);
-	} */
-    
-    pthread_t app_thread;
-    struct spdk_app_opts opts = {};
-    spdk_app_opts_init(&opts, sizeof(opts));
-    opts.name = "hello_bdev";
-
-    struct spdk_thread *spdk_app_threads[3];
-    struct app_thread_ctx spdk_app_ctx = { .spdk_threads = spdk_app_threads};
-    
-    struct app_start_args app_start_ctx = {
-        .app_start_wrapper_func = TestBackEnd,
-        .opts = &opts,
-        .ctx = &spdk_app_ctx
-    };
-    if ((rc = spdk_app_parse_args(argc, argv, &opts, "b:", NULL, dds_parse_arg,
+	if ((rc = spdk_app_parse_args(argc, argv, &opts, "b:", NULL, dds_parse_arg,
 				      dds_custom_args_usage)) != SPDK_APP_PARSE_ARGS_SUCCESS) {
         printf("spdk_app_parse_args() failed with: %d\n", rc);
 		exit(rc);
 	}
-
-    pthread_create(&app_thread, NULL, app_start_wrapper_func, &app_start_ctx);
-    // rc = spdk_app_start(&opts, TestBackEnd, NULL);  // block until `spdk_app_stop` is called
-    // SPDK_NOTICELOG("spdk_app_start returned with: %d\n", rc);
-
-    SPDK_NOTICELOG("joining app thread...\n");
-    int *app_ret;
-    int join_ret = pthread_join(app_thread, app_ret);
-    SPDK_NOTICELOG("join returned: %d, app_ret: %d\n", join_ret, app_ret);
-
-    /* while (1) {
-        SPDK_NOTICELOG("main thread looping...\n");
-        sleep(1);
-    } */
-
-
+    
+    rc = spdk_app_start(&opts, TestBackEnd, NULL);  // block until `spdk_app_stop` is called
+    SPDK_NOTICELOG("spdk_app_start returned with: %d\n", rc);
     spdk_app_fini();
 }
