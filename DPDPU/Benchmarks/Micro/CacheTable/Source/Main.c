21acd832deb256e74effbc98b95f4405a73e4ccd
Qizhen Zhang <qiz.z@outlook.com>
2023-11-18 00:29:58 +0000

    Multi-threaded test

--- DPDPU/Benchmarks/Micro/CacheTable/Source/Main.c
+++ DPDPU/Benchmarks/Micro/CacheTable/Source/Main.c
@@ -1,12 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <pthread.h>
 
 #include "CacheTable.h"
 #include "Profiler.h"
 
-int main() {
+struct ThreadArg {
+    size_t Items;
+    KeyT *KeyList;
+};
+
+void* LookUpThread(void *Arg) {
+    struct ThreadArg *arg = (struct ThreadArg*)Arg;
+    const size_t items = arg->Items;
+    KeyT *keyList = arg->KeyList;
+
+    for (size_t i = 0; i != items; i++) {
+        LookUpCacheTable(&keyList[i]);
+    }
+
+    return NULL;
+}
+
+int main(int ArgC, const char** ArgV) {
     int result = 0;
     const size_t totalItems = 20000000;
+    const size_t lookUpItems = 10000000;
+
+    if (ArgC != 2) {
+        printf("Usage: %s [num threads]\n", ArgV[0]);
+	return -1;
+    }
+
+    int numThreads = atoi(ArgV[1]);
+
+    printf("Cache item size = %ld\n", sizeof(CacheItemT));
 
     fprintf(stdout, "Allocating test items...\n");
     CacheItemT* items = malloc(totalItems * sizeof(CacheItemT));
@@ -47,6 +75,7 @@ int main() {
     fprintf(stdout, "%ld / %ld items added to the cache table\n", itemCount, totalItems);
     ReportProfiler(&profiler);
 
+    /*
     CacheItemT* itemQueried;
     fprintf(stdout, "Testing if added items are correct\n");
     for (size_t i = 0; i != itemCount; i++) {
@@ -58,21 +87,51 @@ int main() {
             fprintf(stderr, "Test failed: Key %ld (%ld -> %ld)\n", items[i].Key, items[i].Offset, itemQueried->Offset);
         }
     }
+    */
 
     fprintf(stdout, "Looking up from the cache table...\n");
-    itemCount = 0;
-    StartProfiler(&profiler);
-    while (itemCount != totalItems) {
-        itemQueried = LookUpCacheTable(&items[itemCount].Key);
-        itemCount++;
+
+    srand(0);
+
+    KeyT** keyLists = malloc(sizeof(KeyT*) * numThreads);
+    struct ThreadArg* threadArgs = malloc(sizeof(struct ThreadArg) * numThreads);
+
+    for (int i = 0; i != numThreads; i++) {
+        keyLists[i] = malloc(sizeof(KeyT) * lookUpItems);
+	for (int j = 0; j != lookUpItems; j++) {
+            keyLists[i][j] = rand() % totalItems;
+	}
+        
+	threadArgs[i].Items = lookUpItems;
+	threadArgs[i].KeyList = keyLists[i];
     }
-    StopProfiler(&profiler);
-    profiler.Operations = itemCount;
+
+    pthread_t* threadIds = malloc(sizeof(pthread_t) * numThreads);
+
+    StartProfiler(&profiler);
+
+    for (int i = 0; i != numThreads; i++) {
+        pthread_create(&threadIds[i], NULL, LookUpThread, &threadArgs[i]);
+    }
     
-    fprintf(stdout, "%ld items looked up\n", itemCount);
+    for (int i = 0; i != numThreads; i++) {
+        pthread_join(threadIds[i], NULL);
+    }
+
+    StopProfiler(&profiler);
+    profiler.Operations = numThreads * lookUpItems;
+    
     ReportProfiler(&profiler);
 
     DestroyCacheTable();
+
+    for (int i = 0; i != numThreads; i++) {
+        free(keyLists[i]);
+    }
+
+    free(threadArgs);	
+    free(threadIds);
+    free(keyLists);
 
     return 0;
 }
