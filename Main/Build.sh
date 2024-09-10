03feb4b09409635f44c2e5c942cbd85a4b46993a
root <qiz.z@outlook.com>
2024-09-10 22:23:11 +0000

    patch dpdk

--- Main/Build.sh
+++ Main/Build.sh
@@ -1,5 +1,5 @@
 # Apply patches
-cp ../NetworkEngine/Patches/dpdk_utils.c /opt/mellanox/doca/applications/common/src/dpdk_utils.c
+patch /opt/mellanox/doca/applications/common/src/dpdk_utils.c ../NetworkEngine/Patches/dpdk_utils.patch
 
 BUILD_DIR="./build/"
 if [ -d "$BUILD_DIR" ]; then
